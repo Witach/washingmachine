f26ea8c585c10b593e1c53c97448936bca35f737
Dawid <215920@edu.p.lodz.pl>
2020-05-17 21:56:40 +0200

    behavioral tests implemented

--- src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,6 +36,16 @@ class WashingMachineTest {
         @Test
         void shouldEndWashingWithSuccessStatus() {
             LaundryBatch laundryBatch = getNotTooHeavyBatch();
+            ProgramConfiguration programConfiguration = createProgramConfiguration();
+
+            LaundryStatus returnedLaundryStatus = washingMashine.start(laundryBatch, programConfiguration);
+
+            assertEquals(ErrorCode.NO_ERROR, returnedLaundryStatus.getErrorCode());
+        }
+
+        @Test
+        void shouldEndWashingWithNotToHeavyLightMaterial(){
+            LaundryBatch laundryBatch = getNotTooHeavyBatchWithLightMaterial();
             ProgramConfiguration programConfiguration = createProgramConfiguration();
 
             LaundryStatus returnedLaundryStatus = washingMashine.start(laundryBatch, programConfiguration);
@@ -118,6 +130,69 @@ class WashingMachineTest {
         }
     }
 
+    @Nested
+    class WashingMachineBehaviourTest{
+
+
+        @Test
+        void shouldWashInCorrectSequenceWithStaticProgram() throws WaterPumpException, EngineException {
+            LaundryBatch laundryBatch = getNotTooHeavyBatch();
+            ProgramConfiguration programConfiguration = createProgramConfiguration();
+
+            washingMashine.start(laundryBatch, programConfiguration);
+
+            InOrder inOrder = inOrder(dirtDetector, engine, waterPump);
+            inOrder.verify(waterPump).pour(anyDouble());
+            inOrder.verify(engine).runWashing(anyInt());
+            inOrder.verify(waterPump).release();
+            inOrder.verify(engine).spin();
+        }
+
+        @Test
+        void shouldWashInCorrectSequenceWithAutoProgram() throws WaterPumpException, EngineException {
+            LaundryBatch laundryBatch = getNotTooHeavyBatch();
+            ProgramConfiguration programConfiguration = createAutoProgramConfiguration();
+
+            makeDirtDetectorReturnPercentageOfDirt(40);
+
+            washingMashine.start(laundryBatch, programConfiguration);
+
+            InOrder inOrder = inOrder(dirtDetector, engine, waterPump);
+            inOrder.verify(dirtDetector).detectDirtDegree(any());
+            inOrder.verify(waterPump).pour(anyDouble());
+            inOrder.verify(engine).runWashing(anyInt());
+            inOrder.verify(waterPump).release();
+            inOrder.verify(engine).spin();
+        }
+
+        @Test
+        void shouldNotSpinIfProgramIsWithoutSpin() throws EngineException {
+            LaundryBatch laundryBatch = getNotTooHeavyBatch();
+
+            ProgramConfiguration programConfiguration = ProgramConfiguration.builder()
+                    .withSpin(false)
+                    .withProgram(Program.LONG)
+                    .build();
+
+            washingMashine.start(laundryBatch, programConfiguration);
+
+            verify(engine,times(0)).spin();
+
+        }
+
+        @Test
+        void shouldNotStartWashWhenTooHeavy(){
+            LaundryBatch laundryBatch = getTooHeavyBatchWithHeavyMaterial();
+
+            ProgramConfiguration programConfiguration = createProgramConfiguration();
+
+            washingMashine.start(laundryBatch, programConfiguration);
+            verifyNoInteractions(engine,dirtDetector,waterPump);
+        }
+
+    }
+
+
     void makeWaterPompFaulty(WaterPump waterPump) throws WaterPumpException {
         doThrow(new WaterPumpException())
                 .when(waterPump)
@@ -132,6 +207,13 @@ class WashingMachineTest {
     LaundryBatch getNotTooHeavyBatch() {
         return LaundryBatch.builder()
                 .withMaterialType(Material.JEANS)
+                .withWeightKg(3)
+                .build();
+    }
+
+    LaundryBatch getNotTooHeavyBatchWithLightMaterial() {
+        return LaundryBatch.builder()
+                .withMaterialType(Material.DELICATE)
                 .withWeightKg(3)
                 .build();
     }
@@ -167,6 +249,7 @@ class WashingMachineTest {
                 .withSpin(true)
                 .build();
     }
+
 
     void makeDirtDetectorReturnPercentageOfDirt(double percentValue) {
         when(dirtDetector.detectDirtDegree(any()))
